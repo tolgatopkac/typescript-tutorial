9049ed145f604916c84a24bf6b0146297cc273a6
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-08-14 03:05:37 +0300

    rename folders name and complete typescript compiler section

---
+++ udemy-colt-steele/02-configuringTypeScript/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const norma = {
+    breed: "Silkie",
+    eggsPerWeek: 4,
+    name: "Norma",
+    age: 2,
+};
+const juniper = {
+    breed: "Silkie",
+    eggsPerWeek: 4,
+    name: "Juniper",
+    age: 1,
+};
---
+++ udemy-colt-steele/02-configuringTypeScript/src/index.ts
@@ -0,0 +1,20 @@
+interface Chicken {
+  breed: string;
+  eggsPerWeek: number;
+  name: string;
+  age: number;
+}
+
+const norma: Chicken = {
+  breed: "Silkie",
+  eggsPerWeek: 4,
+  name: "Norma",
+  age: 2,
+};
+
+const juniper: Chicken = {
+  breed: "Silkie",
+  eggsPerWeek: 4,
+  name: "Juniper",
+  age: 1,
+};
