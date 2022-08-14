10f3d25b3ce757259e4713cde03ee6d0054ed923
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-08-14 06:06:38 +0300

    complete todo miniproject

---
+++ udemy-colt-steele/03-miniproject/dist/index.js
@@ -0,0 +1,47 @@
+"use strict";
+/* // Optional Null (!) - (?)
+const btn = document.getElementById("btn")!;
+btn.addEventListener("click", function () {
+  alert("Clicked");
+}); */
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+const todos = ([] = readTodos());
+todos.forEach(createTodo);
+// local storage
+function readTodos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
+    input.value = "";
+}
+function createTodo(todo) {
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLI.append(todo.text);
+    newLI.append(checkbox);
+    list.append(newLI);
+}
+form.addEventListener("submit", handleSubmit);
---
+++ udemy-colt-steele/03-miniproject/src/index.ts
@@ -0,0 +1,78 @@
+/* // Optional Null (!) - (?)
+const btn = document.getElementById("btn")!;
+btn.addEventListener("click", function () {
+  alert("Clicked");
+}); */
+
+/* // Type ASsertions
+let mystery: unknown = 4;
+const numChars = (mystery as string).length;
+mystery;
+ */
+
+// ----------------------------------------------------------------
+// TYPE ASSERTIONS
+/* const btn = document.getElementById("btn")!;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+
+(<HTMLInputElement>input).value; // HTMLInputElement
+// Doesn't work JSX
+
+btn.addEventListener("click", function () {
+  alert(input.value);
+  input.value = "";
+});
+ */
+
+// ----------------------------------------------------------------
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
+const todos: Todo[] = ([] = readTodos());
+todos.forEach(createTodo);
+
+// local storage
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  saveTodos();
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
+  const newLI = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener("change", function () {
+    todo.completed = checkbox.checked;
+    saveTodos();
+  });
+
+  newLI.append(todo.text);
+  newLI.append(checkbox);
+  list.append(newLI);
+}
+
+form.addEventListener("submit", handleSubmit);
